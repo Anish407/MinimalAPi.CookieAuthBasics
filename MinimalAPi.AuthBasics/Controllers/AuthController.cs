1e26fed0f6b09437434261d2ef7f73e63ce337a9
Anish <anish.arvind407@gmail.com>
2023-01-16 21:48:14 +0100

    added middleware similar to app.useAuthentication

--- MinimalAPi.AuthBasics/Controllers/AuthController.cs
+++ MinimalAPi.AuthBasics/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -54,8 +55,15 @@ namespace MinimalAPi.AuthBasics.Controllers
             // so we use this api to encrypt/decrypt a cookie
             // this is used internally during the authentication process
 
+            //services.AddAuthentication().AddCookie() does a similar thing,
+            // it creates a cookie using the data protection apis and sets it in the response header
+
+            // give this a name, the same name should be used to decrpyt the cookie
             var protector = dataProtectionProvider.CreateProtector("cookie1");
+            // we set the cookie in the response header
             httpContext.Response.Headers["set-cookie"] = $"name={protector.Protect("Anish")}";
+
+            // 
 
             return Ok();
         }
@@ -63,11 +71,24 @@ namespace MinimalAPi.AuthBasics.Controllers
         [HttpGet("ReadCookie")]
         public async Task<IActionResult> ReadCookie()
         {
+            //But every time we cannot write this code to read the cookie contents,
+            //So microsoft has created a
+            //middleware that decrypts the cookie and creates an object that will
+            //represent the logged in user. look at program.cs, we 
+            // have created a middleware
             var protector = dataProtectionProvider.CreateProtector("cookie1");
             var cookie = httpContext.Request.Headers.Cookie.FirstOrDefault(i => i.StartsWith("name="));
             var encryptedData = cookie.Split("=").Last();
             var data = protector.Unprotect(encryptedData);
             return Ok(data);
+        }
+
+        [HttpGet("ReadFromClaims")]
+        public async Task<IActionResult> ReadFromClaims()
+        {
+            // Either we write the code to decrypt or we use the middleware that decrpyts the token and 
+            // 
+            return Ok(httpContext.User);
         }
 
     }
--- MinimalAPi.AuthBasics/Program.cs
+++ MinimalAPi.AuthBasics/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +14,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddDataProtection();
+
 builder.Services.AddAuthentication()
     .AddCookie("mycookie");
 
@@ -25,9 +29,38 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// similar to what app.useAuthentication() works
+app.Use(async (context, next) =>
+{
+    var idp = context.RequestServices.GetRequiredService<IDataProtectionProvider>();
+    var protector = idp.CreateProtector("cookie1");
 
-app.UseAuthentication();
+    // read the data in the cookie from the headers
+    var cookie = context.Request.Headers.Cookie.FirstOrDefault(i => i.StartsWith("name="));
+    if (!string.IsNullOrEmpty(cookie))
+    {
+        var encryptedData = cookie.Split("=").Last();
+        var data = protector.Unprotect(encryptedData);
+
+        var claims = new List<Claim>()
+        {
+            // assign the value read from the cookie to a claim
+            new Claim("name",data),
+            new Claim("age","34"),
+            new Claim("email","a@b.com"),
+        };
+        var identity = new ClaimsIdentity(claims);
+
+        //the schema name will let us know which schema to use and how to
+        // decrypt the cookie
+        //var identity = new ClaimsIdentity(claims, "schema_name");
+        context.User = new ClaimsPrincipal(identity);
+    }
+
+
+    await next(context);
+
+});
 
 app.MapControllers();
 
